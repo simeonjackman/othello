5321f38252f3957b43faf746d8a6ba27115e7bf3
Simeon Jackman <simeon@jackman.ch>
2026-06-03 16:56:04 +0200

    ADD: king_silas_bot

--- othello.py
+++ othello.py
@@ -547,6 +547,46 @@ def der_fels_bot(player, game_board=board):
     return best_move
 
 ###
+#  King Silas Bot
+###
+def king_silas_bot(player, game_board=board):
+    legal_moves = get_legal_moves(player, game_board)
+    # Wenn wir keine Züge haben, müssen wir passen
+    if len(legal_moves) == 0:
+        apply_move(player, None, game_board)
+        return "Pass"
+        
+    # Wir nehmen den Zug, bei dem der score maximal ist.
+    best_move = None
+    best_score = -1000
+
+    # Wir prüfen jeden legalen Zug und bewerten ihn
+    for move in legal_moves:
+        move_score = 0
+        board_after_potential_move = board_after_move(player, move, game_board=board)
+
+        # Wir berechnen, wieviele Steine wir gedreht haben
+        stones_turned = stone_count(opponent(player), board_after_potential_move) - stone_count(opponent(player), board)
+        move_score += stones_turned * 2
+
+        # Wir berechnen, ob Safestones dazubekommen sind
+        if safestones(player, board_after_potential_move) > safestones(player, board):
+            move_score += 10
+        # Wir evalieren wo wir unseren Stein hinsetzen
+        if move_position(move) == "corner":
+            move_score += 15
+        if move_position(move) == "edge":
+            move_score += 6
+
+        # Ist der aktuelle Zug der Beste?
+        if move_score > best_score:
+            best_score = move_score
+            best_move = move
+        
+    apply_move(player, best_move, game_board)
+    return best_move
+
+###
 # Hier können Bots zum spielen gewählt werden
 # Zur Auswahl stehen alle, welche im Code definiert wurden
 # 1. manual_play: Manuell spielen
@@ -557,7 +597,7 @@ def der_fels_bot(player, game_board=board):
 # 6. tensai_bot
 # 7. the_big_uttige_bot
 # 8. i_ah_bot
-# 9. der_fels_bot
+# 9. king_silas_bot
 ###
 
 
